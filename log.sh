bf885b8f81bc7028642e0a04ed26079b55620415
gammamonbiscuit <58557683+gammamonbiscuit@users.noreply.github.com>
2026-04-06 17:07:25 +0800

    Added environment variable  to toggle this on and off

--- log.sh
+++ log.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-until compgen -G "/var/log/terraria/Log_*.log" > /dev/null; do
-    sleep 1
-    echo "Waiting for log file"
-done
-tail -n +1 -f /var/log/terraria/Log_*.log>${TERRARIA_DIR}/Logs/${world%.*}.log
+if [[ $log == 1 || $log == true ]]; then
+    until compgen -G "/var/log/terraria/Log_*.log" > /dev/null; do
+        sleep 1
+        echo "Waiting for log file"
+    done
+    tail -n +1 -f /var/log/terraria/Log_*.log>${TERRARIA_DIR}/Logs/${world%.*}.log
+fi
