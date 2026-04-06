e7aabad487b9803837a2189afaad8a4eb29bb1aa
gammamonbiscuit <58557683+gammamonbiscuit@users.noreply.github.com>
2026-04-06 16:54:39 +0800

    Use world name for the log file instead of server.log

--- log.sh
+++ log.sh
@@ -3,4 +3,4 @@ until compgen -G "/var/log/terraria/Log_*.log" > /dev/null; do
     sleep 1
     echo "Waiting for log file"
 done
-tail -n +1 -f /var/log/terraria/Log_*.log>${TERRARIA_DIR}/Logs/server.log
+tail -n +1 -f /var/log/terraria/Log_*.log>${TERRARIA_DIR}/Logs/${world%.*}.log
